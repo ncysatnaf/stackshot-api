c0d55ce17a1987f8eded47cc91d8a739a9baaf5e
vai <ncysatnaf@gmail.com>
2016-11-17 17:08:19 +0800

    base socket with test

--- src/handle-socket.js
+++ src/handle-socket.js
@@ -1,7 +1,30 @@
 export default server => {
   global.io = require('socket.io')(server)
+
   io.on('connection', socket => {
-    console.log('connection')
+
+    // console.log('connection')
+
+    socket.on('join', (data) => {
+      const {uid} = data
+
+      console.dir(`[user]${uid} joined`, {colors: true})
+      socket.join(uid)
+    })
+
+    socket.on('message', data => {
+
+      data === 'hello' && socket.emit('message','hi')
+    })
+
+    socket.on('new-reply', data => {
+      const { uid, to, content } = data
+
+      console.dir(`[user]${uid} reply [user]${to} with [content]${content}`, {colors: true})
+      io.sockets.to(to).emit('message', {message: 'new-reply', uid, content})
+    })
+
   })
+
   return io
 }
