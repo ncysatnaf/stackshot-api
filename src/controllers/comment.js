b24870f5635515d257a8b8c8caef892fff9bff85
vai <ncysatnaf@gmail.com>
2016-11-17 20:38:47 +0800

    socket doc api

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -4,9 +4,33 @@ import Shot from '../models/shot'
 import {validate} from '../common/helpers'
 import mongoose from 'mongoose'
 
+
+export async function getReplys(ctx) {
+  const {limit = 10, before, after} = ctx.query
+  const {sub} = ctx.state.user
+
+  const query = { replyTo: mongoose.Types.ObjectId(sub) }
+  if (before) {
+    query.createdAt = {$lt: new Date(before)}
+  }
+  if (after) {
+    query.createdAt = {$gt: new Date(after)}
+  }
+
+  const replys = await Comment
+    .find(query)
+    .sort('createdAt')
+    .limit(parseInt(limit, 10))
+    .populate('user', 'username avatar')
+    .populate('shot', '_id')
+    .populate('replyTo', 'username avatar')
+    .exec()
+
+  ctx.body = replys
+}
+
 export async function getComments(ctx) {
   const {limit = 10, before, after, shot} = ctx.query
-  const {sub} = ctx.state.user || {}
 
   const query = { shot: mongoose.Types.ObjectId(shot) }
   if (before) {
--- src/handle-socket.js
+++ src/handle-socket.js
@@ -1,27 +1,27 @@
+const online = {}
+
 export default server => {
   global.io = require('socket.io')(server)
 
   io.on('connection', socket => {
 
-    // console.log('connection')
+    console.log('connection')
 
     socket.on('join', (data) => {
       const {uid} = data
 
-      console.dir(`[user]${uid} joined`, {colors: true})
+      this.online[uid] = uid
+      // console.dir(`[user]${uid} joined`, {colors: true})
       socket.join(uid)
     })
 
-    socket.on('message', data => {
-
-      data === 'hello' && socket.emit('message','hi')
-    })
-
     socket.on('new-reply', data => {
-      const { uid, to, content } = data
+      const { uid, to, comment } = data
 
-      console.dir(`[user]${uid} reply [user]${to} with [content]${content}`, {colors: true})
-      io.sockets.to(to).emit('message', {message: 'new-reply', uid, content})
+      if(this.online[to]){
+        // console.dir(`[user]${uid} reply [user]${to} with [comment]${JSON.stringify(comment)}`, {colors: true})
+        io.sockets.to(to).emit('message', {message: 'new-reply', uid, comment})
+      }
     })
 
   })
